56d53f661cf18ee41a6f46968da9d0926d4e5ab4
BMK-JAVA <bmk_92@mail.ru>
2021-06-14 09:57:16 +0600

    initial commit

--- src/main/java/kz/bcc/tutorial/balatime/model/dto/StudentUser/LessonItem.java
+++ src/main/java/kz/bcc/tutorial/balatime/model/dto/StudentUser/LessonItem.java
@@ -7,4 +7,10 @@ public class LessonItem {
     private String name;
     private String teacher;
     private String group;
+    private String Room;
+    private Integer day;
+    private boolean free;
 }
+
+
+
